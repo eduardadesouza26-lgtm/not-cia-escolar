dc03066b8692883e87380ed38aa63655a34352a2
eduardadesouza26-lgtm <eduarda.de.souza26@escola.pr.gov.br>
2026-08-12 10:43:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let count = 0;
+const likeBtn = document.getElementById('likeBtn');
+const likesSpan = document.getElementById('likes');
 
+likeBtn.addEventListener('click', () => {
+    count++;
+    likesSpan.textContent = count;
+});
